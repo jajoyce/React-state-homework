f644765f341fed0c866eedf8ab25e4102fd1169b
John A. Joyce <48220338+jajoyce@users.noreply.github.com>
2022-02-24 23:53:18 -0500

    Complete deliverable. Add useState, create bigImage, add handleClick function to setBigImage. Render bigImage in app and update on click.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./styles.css";
 import imagesArr from "./imageData";
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage]
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const [bigImage, setBigImage] = useState({
+    src:
+      "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    alt: "London"
+  });
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  function handleClick(img, city) {
+    setBigImage({
+      src: img,
+      alt: city
+    });
+  }
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
   const images = imagesArr.map((elem, index) => {
     return (
       <img
@@ -18,7 +23,7 @@ export default function App() {
         src={elem.img}
         alt={elem.city}
         key={index}
-        onclick=""
+        onClick={() => handleClick(elem.img, elem.city)}
       />
     );
   });
@@ -28,8 +33,7 @@ export default function App() {
       <h1>Cities Of The World</h1>
       <div id="wrapper">
         <div id="thumbnails">{images}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        <img id="bigimage" src={bigImage.src} alt={bigImage.alt} />
       </div>
     </div>
   );
